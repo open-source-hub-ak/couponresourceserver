7873e683972ed7af1a9f1c0d32ba0d18f36bedbe
aakash <aakash>
2021-02-03 01:03:00 +0530

    # resource server will verify token using public key and set verifier method

--- src/main/java/com/opensource/couponresourceserver/security/ResourceServerConfig.java
+++ src/main/java/com/opensource/couponresourceserver/security/ResourceServerConfig.java
@@ -1,17 +1,27 @@
 package com.opensource.couponresourceserver.security;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import com.opensource.couponresourceserver.util.SecurityConstants;
 
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	@Value("${publicKey}")
+	private String publicKey;
 
 	// which resource it is protecting;
 	@Override
@@ -27,5 +37,19 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		http.authorizeRequests().mvcMatchers(HttpMethod.GET, "/couponapi/coupons/{code:^[A-Z]*$}")
 				.hasAnyRole("USER", "ADMIN").mvcMatchers(HttpMethod.POST, "/couponapi/coupons").hasRole("ADMIN")
 				.anyRequest().denyAll().and().csrf().disable();
+	}
+
+//	 resource server will verify token using public key
+	@Bean
+	public TokenStore tokenStore() {
+		return new JwtTokenStore(jwtAccessTokenConverter());
+	}
+
+	@Bean
+	public JwtAccessTokenConverter jwtAccessTokenConverter() {
+
+		JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+		jwtAccessTokenConverter.setVerifierKey(publicKey);
+		return jwtAccessTokenConverter;
 	}
 }
--- src/test/java/com/opensource/couponresourceserver/CouponresourceserverApplicationTests.java
+++ src/test/java/com/opensource/couponresourceserver/CouponresourceserverApplicationTests.java
@@ -1,5 +1,20 @@
 package com.opensource.couponresourceserver;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
+import java.util.Base64;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +22,33 @@ import org.springframework.boot.test.context.SpringBootTest;
 class CouponresourceserverApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException, UnrecoverableKeyException {
+		
+			String url ="E:\\\\WS\\\\OAuth2server\\\\src\\\\main\\\\resources\\\\jwtiscool.jks";
+	        FileInputStream is = new FileInputStream(url);
+
+	        KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+	        keystore.load(is, "jwtiscool".toCharArray());
+
+	        String alias = "jwtiscool";
+
+	        Key key = keystore.getKey(alias, "jwtiscool".toCharArray());
+	        if (key instanceof PrivateKey) {
+	          // Get certificate of public key
+	          Certificate cert = keystore.getCertificate(alias);
+
+	          // Get public key
+	          PublicKey publicKey = cert.getPublicKey();
+
+	          // Return a key pair
+	          new KeyPair(publicKey, (PrivateKey) key);
+	          
+	          byte[] encodedPublicKey = publicKey.getEncoded();
+	          String b64PublicKey = Base64.getMimeEncoder().encodeToString(encodedPublicKey);
+	          
+	          System.out.println(b64PublicKey);
+	        }
+	      
 	}
 
 }
