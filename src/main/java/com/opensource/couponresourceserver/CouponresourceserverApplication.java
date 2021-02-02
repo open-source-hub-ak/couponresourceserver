a6397b90f7133efd153cded1707689e41fe0d623
aakash <aakash>
2021-02-02 23:16:16 +0530

    creating separate resource server from coupon service

---
+++ src/main/java/com/opensource/couponresourceserver/CouponresourceserverApplication.java
@@ -0,0 +1,13 @@
+package com.opensource.couponresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponresourceserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponresourceserverApplication.class, args);
+	}
+
+}
